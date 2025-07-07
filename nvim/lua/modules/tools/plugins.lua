a1e033c9fa342f4df96bd18cd7b18a80dcd00036
probe2k <kyash054@gmail.com>
2025-07-08 02:55:59 +0530

    add nvim-notify

--- nvim/lua/modules/tools/plugins.lua
+++ nvim/lua/modules/tools/plugins.lua
@@ -23,4 +23,16 @@ return function(tools)
 		config = conf.git,
 		lazy = true,
 	})
+
+	tools({
+		'rcarriga/nvim-notify',
+		config = function ()
+			require('notify').setup {
+				stages = 'fade_in_slide_out',
+				background_colour = 'FloatShadow',
+				timeout = 2000,
+			}
+			vim.notify = require('notify')
+		end
+	})
 end
